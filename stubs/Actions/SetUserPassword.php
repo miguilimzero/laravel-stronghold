b935d14cb14bc2c2763424204837be4bb58674e0
miguilimzero <35383529+miguilimzero@users.noreply.github.com>
2025-09-20 21:57:10 -0300

    Update SetUserPassword.php

--- stubs/Actions/SetUserPassword.php
+++ stubs/Actions/SetUserPassword.php
@@ -3,12 +3,8 @@
 namespace App\Actions\Fortify;
 
 use App\Models\User;
-
-use App\Actions\Fortify\PasswordValidationRules;
-
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
-
 use Miguilim\LaravelStronghold\Contracts\SetsUserPasswords;
 
 class SetUserPassword implements SetsUserPasswords
